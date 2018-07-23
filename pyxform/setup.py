81b6829ab71239fcdc7308fb52b71140f3c75e71
Stephen Quan <squan@esri.com>
2018-07-24 08:59:09 +1000

    Initial check in

---
+++ pyxform/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""Pyxform - Python library that converts XLSForms to XForms.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='pyxform',
+    version='0.11.5',
+    author='github.com/xlsform',
+    author_email='info@xlsform.org',
+    packages=find_packages(),
+    package_data={
+        'pyxform.validators.odk_validate': [
+            'bin/*.*',
+        ],
+        'pyxform.tests': [
+            'example_xls/*.*',
+            'fixtures/strings.ini',
+            'bug_example_xls/*.*',
+            'test_output/*.*',
+            'test_expected_output/*.*',
+            'validators/.*',
+            'validators/data/*.*',
+            'validators/data/.*',
+        ],
+        'pyxform': [
+            'iana_subtags.txt',
+        ]
+    },
+    url='http://pypi.python.org/pypi/pyxform/',
+    description='A Python package to create XForms for ODK Collect.',
+    long_description=open('README.rst', 'rt').read(),
+    install_requires=[
+        'xlrd>=1.1.0',
+        'unicodecsv>=0.14.1',
+        'formencode',
+        'unittest2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'xls2xform=pyxform.xls2xform:main_cli',
+            'pyxform_validator_update=pyxform.validators.updater:main_cli'
+        ],
+    },
+)
---
+++ xls2xform-api.py
@@ -0,0 +1,43 @@
+import sys, subprocess, json, tempfile, shutil, os, traceback
+from flask import Flask, request
+from flask_restful import reqparse, Resource, Api
+
+def pythonic_path(path):
+    return path if os.path.sep == '/' else path.replace(os.path.sep, '/')
+
+class Xls2XForm(Resource):
+    def post(self):
+        temp_dir = tempfile.mkdtemp()
+        try:
+            file = request.files['xlsform']
+            temp_xlsx = os.path.join(temp_dir, file.filename)
+            temp_xml = os.path.join(temp_dir, file.filename + '.xml')
+            file.save(temp_xlsx)
+            print('Converting', temp_xlsx, file=sys.stderr)
+            out = subprocess.check_output( [
+                'python',
+                'pyxform/pyxform/xls2xform.py',
+                '--json',
+                pythonic_path(temp_xlsx),
+                pythonic_path(temp_xml)
+            ] )
+            result = json.loads(out)
+            print(result, file=sys.stderr)
+            if os.path.exists(temp_xml):
+                with open(temp_xml, 'r') as xml:
+                    result['xform'] = xml.read()
+            shutil.rmtree(temp_dir)
+            return result
+        except Exception as e:
+            shutil.rmtree(temp_dir)
+            print(str(e), file=sys.stderr)
+            return { 'code': 999, 'message': traceback.format_exc() }
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Xls2XForm, '/api/xls2xform')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5123, debug=True)
+
